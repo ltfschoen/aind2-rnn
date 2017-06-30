edd001d167e825df2a79b4a440d6ca7a10312f02
Luke Schoen <ltfschoen@gmail.com>
2017-06-30 19:14:59 +1000

    feat: Add simple RNN model using Keras for regression

--- my_answers.py
+++ my_answers.py
@@ -25,9 +25,35 @@ def window_transform_series(series,window_size):
     
     return X,y
 
-# TODO: build an RNN to perform regression on our time series input/output data
+# DONE: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(step_size, window_size):
-    pass
+    ### DONE: create required RNN model
+
+    # given - fix random seed - so we can all reproduce the same results on our default time series
+    np.random.seed(0)
+
+    # DONE: build an RNN to perform regression on our time series input/output data
+
+    # layer 1 of Sequential model uses LSTM module with:
+    # - 5 hidden units
+    # - input_shape = (window_size,1))
+    model = Sequential()
+    model.add(LSTM(5, input_shape=(window_size, 1)))
+
+    # layer 2 uses a fully connected module with one unit
+    model.add(Dense(1))
+
+    # build model using keras documentation recommended optimizer initialization
+    optimizer = keras.optimizers.RMSprop(lr=0.001, 
+                                         rho=0.9, 
+                                         epsilon=1e-08,
+                                         decay=0.0)
+
+    # compile the model
+    model.compile(loss='mean_squared_error', 
+                  optimizer=optimizer)
+
+    return model
 
 
 ### TODO: list all unique characters in the text and remove any non-english ones
